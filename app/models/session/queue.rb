e27111b033bef9dbf0023ebbabc5f7f700c608b9
Jon Stokes <jon@jonstokes.com>
2015-07-31 15:08:48 -0500

    Session pop works

--- app/models/session/queue.rb
+++ app/models/session/queue.rb
@@ -3,23 +3,48 @@ module Session
     self.table_name = 'session_queues'
 
     belongs_to :rate_limit
-    has_many   :sessions, class_name: "Session:Session"
+    has_many   :sessions, class_name: "Session::Session"
 
     validates :name,          presence: true
     validates :max_size,      presence: true, numericality: { greater_than: 0 }
     validates :rate_limit_id, presence: true
 
     def pop
-      # atomic pop from queue
+      with_limit { shift }
+    end
+
+    def push(session)
+      session.session_queue = self
+      db { session.save! }
+      session
+    end
+    alias_method :add, :push
+
+    def size
+      sessions.count
+    end
+
+    def sessions
+      db { Session.where(session_queue_id: id) }
     end
 
     def with_limit
-      rate_limiter.with_limit(limiter_key) do
+      rate_limit.with_limit(limiter_key) do
         yield
       end
     end
 
     private
+
+    def shift
+      db do |conn|
+        next unless record = sessions.lock(true).first
+        record.destroy
+        record
+      end
+    end
+
+    def db(&b); ActiveRecord::Base.connection_pool.with_connection(&b); end
 
     def limiter_key
       "#{self.class.name}::#{name}"
--- spec/models/session/session_queue_spec.rb
+++ spec/models/session/session_queue_spec.rb
@@ -18,4 +18,27 @@ RSpec.describe Session::Queue, type: :model do
       expect(subject).not_to be_valid
     end
   end
+
+  context "pusing and popping" do
+    let(:session_queue) { create(:session_queue) }
+    
+    describe "#push" do
+      it "adds a session object to the session queue" do
+        session = build(:session)
+        session_queue.push(session)
+        expect(session_queue.size).to eq(1)
+      end
+    end
+
+    describe "#pop" do
+      it "removes a session from the queue" do
+        session = build(:session)
+        session_queue.push(session)
+
+        ssn = session_queue.pop
+        expect(ssn).to be_a(Session::Session)
+        expect(ssn).not_to be_persisted
+      end
+    end
+  end
 end
