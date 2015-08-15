4837e4818bbeefea9e84624f79c9ebbcd52493ed
Jon Stokes <jon@jonstokes.com>
2015-08-15 14:58:29 -0500

    Fixes to example validation json

--- examples/news/normalizations.rb
+++ examples/news/normalizations.rb
@@ -1,7 +1,7 @@
 Extension.define "global/normalizations" do
   extension do
     def normalize_time(str)
-      Time.parse(str) if str.present?
+      Time.parse(str).to_datetime.rfc3339 if str.present?
     end
   end
 end
