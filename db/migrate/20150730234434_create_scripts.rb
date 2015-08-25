7f89955bfa54dfbd91f3cece46d3b61e096f0ec5
Jon Stokes <jon@jonstokes.com>
2015-08-24 20:27:14 -0500

    Remove unused migrations and schema

--- db/migrate/20150730234434_create_scripts.rb
+++
@@ -1,9 +0,0 @@
-class CreateScripts < ActiveRecord::Migration
-  def change
-    create_table :scripts, id: :uuid, default: "uuid_generate_v4()" do |t|
-      t.string :name,   null: false
-      t.text   :source, null: false
-      t.timestamps      null: false
-    end
-  end
-end
--- db/migrate/20150730234440_create_extensions.rb
+++
@@ -1,9 +0,0 @@
-class CreateExtensions < ActiveRecord::Migration
-  def change
-    create_table :extensions, id: :uuid, default: "uuid_generate_v4()" do |t|
-      t.string :name,   null: false
-      t.text   :source, null: false
-      t.timestamps      null: false
-    end
-  end
-end
